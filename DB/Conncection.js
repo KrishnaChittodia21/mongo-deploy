d045c61cb23f9aa1c5e358de33ea9b94548f3dc6
krishna <krishnachittodia.10@gmail.com>
2020-07-30 05:08:15 +0530

    updated code

--- DB/Conncection.js
+++ DB/Conncection.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-
-// const URI ="mongodb+srv://admin:k7503673841@mongo-proj.z7pkk.mongodb.net/mongo-proj?retryWrites=true&w=majority";
-const URI ="mongodb://localhost:27017/mongo-proj";
+require('dotenv').config();
 
 const connectDB = async () => {
-  await mongoose.connect(URI, {
+  await mongoose.connect(process.env.PROD_MONGO_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true
   });
